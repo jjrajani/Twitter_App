fb72c6a6f52a26456e37cacf641c24e476a9b4dc
jjrajani <jjrajani@gmail.com>
2017-12-13 21:36:23 -0500

    move header render to own function

--- client/src/components/TwitterFeed.js
+++ client/src/components/TwitterFeed.js
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types';
 import { TweetItem } from './';
 
 class TwitterFeed extends Component {
+  renderHeader() {
+    const { searchTerm } = this.props;
+    return searchTerm ? (
+      <h2>You searched for {searchTerm}</h2>
+    ) : (
+      <h2>Enter a Search to Get Started</h2>
+    );
+  }
   render() {
-    const { searchTerm, tweets } = this.props;
+    const { tweets } = this.props;
     return (
       <div>
-        {!searchTerm && <h1>Enter a Search to Get Started</h1>}
-        {searchTerm && <h1>You searched for {searchTerm}</h1>}
+        {this.renderHeader()}
         {tweets && (
           <ul>
             {tweets.map(t => {
